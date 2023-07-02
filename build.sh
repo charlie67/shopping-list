e80380cc6579bc5419d9f9f55efc5793145d1636
charlie <charlierobinson080@gmail.com>
2023-07-02 03:57:50 +0100

    change dockerfile dir

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ npm install
 npm run build
 
 echo $2 | docker login -u $1 --password-stdin
-docker build --tag $1/shopping-list:${GITHUB_REF_NAME} -f Dockerfile .
+docker build --tag $1/shopping-list:${GITHUB_REF_NAME} -f Dockerfile ../
 docker push $1/shopping-list:${GITHUB_REF_NAME}
