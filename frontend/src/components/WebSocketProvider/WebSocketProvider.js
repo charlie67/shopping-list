adb94eb097964ce00ee5c37a94426365e29d41c6
Charlie Robinson <charlierobinson080@gmail.com>
2023-09-01 00:20:07 +0100

    Improve Websocket timeouts (#34)
    * Improve Websocket timeouts
    
    * update name for dependabot tag
    
    * update name for dependabot tag

--- frontend/src/components/WebSocketProvider/WebSocketProvider.js
+++ frontend/src/components/WebSocketProvider/WebSocketProvider.js
@@ -3,11 +3,10 @@ import useWebSocket from 'react-use-websocket';
 import {WEBSOCKET_URL} from "../../url_const";
 import {connect, useDispatch} from "react-redux";
 import {
-    SHOPPING_LIST_ITEM_CREATED,
     SHOPPING_LIST_ITEM_DELETED,
     SHOPPING_LIST_ITEM_UPDATED
 } from "../../actionTypes/actionTypes";
-import {shoppingListItemCreated, shoppingListItemDeleted, shoppingListItemUpdated} from "../../actionTypes/actions";
+import {shoppingListItemDeleted, shoppingListItemUpdated} from "../../actionTypes/actions";
 
 // Create the WebSocketContext
 export const WebSocketContext = createContext();
@@ -24,17 +23,13 @@ const WebSocketProvider = ({ children }) => {
         },
         shouldReconnect: () => true,
         retryOnError: true,
-        reconnectAttempts: -1,
+        reconnectAttempts: 9999999,
     });
 
     useEffect(() => {
         // Handle received messages or perform other WebSocket-related tasks here
         if (lastJsonMessage) {
-            console.debug("WS message received", lastJsonMessage);
-
-            if (lastJsonMessage.messageType === SHOPPING_LIST_ITEM_CREATED) {
-                dispatch(shoppingListItemCreated(lastJsonMessage.data));
-            } else if (lastJsonMessage.messageType === SHOPPING_LIST_ITEM_UPDATED) {
+            if (lastJsonMessage.messageType === SHOPPING_LIST_ITEM_UPDATED) {
                 dispatch(shoppingListItemUpdated(lastJsonMessage.data));
             } else if (lastJsonMessage.messageType === SHOPPING_LIST_ITEM_DELETED) {
                 dispatch(shoppingListItemDeleted(lastJsonMessage.data));
