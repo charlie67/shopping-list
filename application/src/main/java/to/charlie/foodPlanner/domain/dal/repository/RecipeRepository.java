797c0a0d11db5e3853157657d93198a0b509dd1b
Charlie Robinson <charlierobinson080@gmail.com>
2025-02-26 02:38:06 +0000

    use java 21

--- application/src/main/java/to/charlie/foodPlanner/domain/dal/repository/RecipeRepository.java
+++ application/src/main/java/to/charlie/foodPlanner/domain/dal/repository/RecipeRepository.java
@@ -1,5 +1,6 @@
 package to.charlie.foodPlanner.domain.dal.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import to.charlie.foodPlanner.domain.model.entity.recipe.RecipeEntity;
@@ -8,4 +9,5 @@ public interface RecipeRepository extends CrudRepository<RecipeEntity, UUID> {
 
   boolean existsByUrl(String url);
 
+  Optional<RecipeEntity> findByUrl(String url);
 }
