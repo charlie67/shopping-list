1d77360318133a36fc09f08776ae8dc3377d560a
Charlie Robinson <charlierobinson080@gmail.com>
2025-07-19 13:18:06 +0100

    fix build

--- frontend/src/url_const.js
+++ frontend/src/url_const.js
@@ -8,5 +8,5 @@ export const SHOPPING_LIST_ADD_ITEM_ENDPOINT = API_BASE + "shoppinglist";
 export const SHOPPINGLIST_PAGEABLE_ENDPOINT = SHOPPING_LIST_BASE + "pageable/";
 
 export const RECIPE_BASE = API_BASE + "recipe";
-export const EXTRACT_RECIPE_ENDPOINT = RECIPE_BASE + "/" + "extract";
+export const EXTRACT_RECIPE_ENDPOINT = RECIPE_BASE + "/extract";
 export const RECIPE_PAGEABLE_ENDPOINT = RECIPE_BASE;
